c5007bed26d6ccfdd32488ed72769491b6cedd3a
Markjust1 <novikov711@gmail.com>
2022-06-15 16:05:35 -0600

    refactor stories and components

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -6,11 +6,11 @@ export default function DayList (props) {
   return (
     <ul>
       {props.days.map((day) => <DayListItem 
-        key={day.id}
-        name={day.name}
-        spots={day.spots}
-        selected={day.name === props.day}
-        onClick={() => {props.setDay(day.id)}}
+        key={day.id} 
+        name={day.name} 
+        spots={day.spots} 
+        selected={day.name === props.value}
+        setDay={props.onChange}
       />)}
     </ul>
   )
